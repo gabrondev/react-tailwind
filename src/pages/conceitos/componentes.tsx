5f2e3f7de42fc1def0afe49d16e4bfd8c6f3fa6f
Gabriel <gabrieltux19@gmail.com>
2025-03-31 05:43:02 -0300

    Exercícios dia 31/03/2025

---
+++ src/pages/conceitos/componentes.tsx
@@ -0,0 +1,9 @@
+export default function Componentes() {
+    return (
+        <div className="flex-center h-screen gap-5">
+            <button className="btn btn-info">Info</button>
+            <button className="btn btn-success">Sucesso</button>
+            <button className="btn btn-error">Erro</button>
+        </div>
+    )
+}
